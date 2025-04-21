24fd62c7b5e3999fa2109d2460cda456e005ce86
rajeevcoding <rajeev7007@hotmail.com>
2025-04-22 00:21:29 +0530

    added consurreny to bank account

--- bank/bankAccount.go
+++ bank/bankAccount.go
@@ -2,28 +2,46 @@ package bank
 
 import (
 	"fmt"
+	"go-hero/logging"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 type BankAccount struct {
 	Balance float64
 	Owner   string
+	mu      sync.RWMutex
+	Logger  logging.Logger
 }
 
 func (ba *BankAccount) Deposit(amount float64) {
 	if amount > 0 {
+		ba.mu.Lock()
+		defer ba.mu.Unlock()
 		ba.Balance += amount
+		ba.Logger.Info(fmt.Sprintf("Deposited %.2f. New balance: %.2f", amount, ba.Balance))
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 	}
 }
 
 func (ba *BankAccount) Withdraw(amount float64) error {
+	ba.mu.Lock()
+	defer ba.mu.Unlock()
 	if ba.Balance < amount {
 		return fmt.Errorf("insufficient balance")
 	}
 
-	ba.Balance -= ba.Balance
+	ba.Balance -= amount
+	ba.Logger.Info(fmt.Sprintf("Withdrawn %.2f. New balance: %.2f", amount, ba.Balance))
+	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 	return nil
 }
 
-func (ba *BankAccount) PrintBalance() {
+func (ba *BankAccount) GetBalance() float64 {
+	ba.mu.RLock()
+	defer ba.mu.RUnlock()
 	fmt.Printf("Available Balance: $%.2f\n", ba.Balance)
+	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	return ba.Balance
 }
--- main.go
+++ main.go
@@ -3,49 +3,91 @@ package main
 import (
 	"fmt"
 	"go-hero/bank"
-	"go-hero/geometry"
 	"go-hero/logging"
-	"go-hero/media"
+
+	// "go-hero/geometry"
+	// "go-hero/logging"
+	// "go-hero/media"
+	"sync"
+	"time"
 )
 
+func PrintNumbers(from string) {
+	for i := 0; i < 5; i++ {
+		fmt.Printf("From %s - %d\n", from, i)
+		time.Sleep(time.Second)
+	}
+}
+
 func main() {
-	rectangle := geometry.Rectangle{Length: 5, Breadth: 4}
-	circle := geometry.Circle{Radius: 14}
+	// rectangle := geometry.Rectangle{Length: 5, Breadth: 4}
+	// circle := geometry.Circle{Radius: 14}
 
-	geometry.PrintShapeDetails(rectangle)
-	geometry.PrintShapeDetails(circle)
+	// geometry.PrintShapeDetails(rectangle)
+	// geometry.PrintShapeDetails(circle)
 
-	acc := bank.BankAccount{Owner: "Ravi", Balance: 1000}
-	acc.PrintBalance()
-	acc.Deposit(500)
-	acc.PrintBalance()
-	acc.Withdraw(200)
-	acc.PrintBalance() // should print: Current balance: 1300
+	// acc := bank.BankAccount{Owner: "Ravi", Balance: 1000}
+	// acc.PrintBalance()
+	// acc.Deposit(500)
+	// acc.PrintBalance()
+	// acc.Withdraw(200)
+	// acc.PrintBalance() // should print: Current balance: 1300
 
-	playlistSongs := []media.Song{
-		{Title: "1", Artist: "A", DurationInSeconds: 300},
-		{Title: "2", Artist: "B", DurationInSeconds: 200},
-		{Title: "3", Artist: "C", DurationInSeconds: 250},
+	// playlistSongs := []media.Song{
+	// 	{Title: "1", Artist: "A", DurationInSeconds: 300},
+	// 	{Title: "2", Artist: "B", DurationInSeconds: 200},
+	// 	{Title: "3", Artist: "C", DurationInSeconds: 250},
+	// }
+
+	// playlist := media.Playlist{Songs: playlistSongs}
+	// playlist.AddSong(media.Song{Title: "4", Artist: "D", DurationInSeconds: 150})
+	// fmt.Println(playlist.TotalDuration())
+	// playlist.PrintSongs()
+
+	// fileLogger, err := logging.NewFileLogger("testlog.log", true)
+	// if err != nil {
+	// 	fmt.Println("error opening the file")
+	// } else {
+	// 	loggers := []logging.Logger{
+	// 		&logging.ConsoleLogger{BaseLogger: logging.BaseLogger{Utc: false}},
+	// 		fileLogger,
+	// 	}
+
+	// 	for _, logger := range loggers {
+	// 		logger.Info("Info Message")
+	// 		logger.Error("Error Message", nil)
+	// 		logger.Error("Error Message with Error", fmt.Errorf("error error error"))
+	// 	}
+	// }
+
+	// go PrintNumbers("First")
+	// go PrintNumbers("Second")
+
+	// time.Sleep(6 * time.Second)
+	// fmt.Println("main function done")
+
+	account := &bank.BankAccount{Owner: "Rajeev", Balance: 1000, Logger: &logging.ConsoleLogger{}}
+
+	var wg sync.WaitGroup
+
+	// Launch 5 concurrent deposits
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			account.Deposit(100)
+		}(i)
 	}
 
-	playlist := media.Playlist{Songs: playlistSongs}
-	playlist.AddSong(media.Song{Title: "4", Artist: "D", DurationInSeconds: 150})
-	fmt.Println(playlist.TotalDuration())
-	playlist.PrintSongs()
-
-	fileLogger, err := logging.NewFileLogger("testlog.log", true)
-	if err != nil {
-		fmt.Println("error opening the file")
-	} else {
-		loggers := []logging.Logger{
-			&logging.ConsoleLogger{BaseLogger: logging.BaseLogger{Utc: false}},
-			fileLogger,
-		}
-
-		for _, logger := range loggers {
-			logger.Info("Info Message")
-			logger.Error("Error Message", nil)
-			logger.Error("Error Message with Error", fmt.Errorf("error error error"))
-		}
+	// Launch 5 concurrent withdrawals
+	for i := 0; i < 75; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			account.Withdraw(50)
+		}(i)
 	}
+
+	wg.Wait()
+	fmt.Printf("Final Balance: $%.2f\n", account.GetBalance())
 }
